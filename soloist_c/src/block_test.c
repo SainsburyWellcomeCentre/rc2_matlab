53a2015e4526f19e284c059a1f18dffb6a56339e
lcossell <l.cossell@ucl.ac.uk>
2019-10-15 14:23:26 +0100

    start handling errors in C files

--- soloist_c/src/block_test.c
+++ soloist_c/src/block_test.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <windows.h>
+
+BOOL WINAPI consoleHandler(DWORD signal) {
+    
+    printf("not handling this");
+    if (signal == CTRL_C_EVENT) {
+        printf("handling this");
+        exit(2);
+    }
+    
+    return 2;
+}
+
 
 int
 main(int argc, char **argv) {
+    
+    if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
+        printf("cannot setup handler");
+        return 1;
+    }
+    
     printf("sleeping for 5 seconds...\n");
     sleep(5);
     printf("done\n");
+    return(0);
 }
