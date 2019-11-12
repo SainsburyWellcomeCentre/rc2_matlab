88538ce71e03faf89e7b5ca11ef5dbae1ff298c1
lcossell <l.cossell@ucl.ac.uk>
2019-11-12 15:55:40 +0000

    improve values of the offsets

--- rc/aux_/plotting_config.m
+++ rc/aux_/plotting_config.m
@@ -3,5 +3,5 @@ function config = plotting_config()
 config.time = 10; %s
 config.fig.position = [1340, 80, 1333, 630];
 config.chans_to_plot = 1:7;
-config.ylim = {[-22, 102], [-22, 102], [-0.1, 5.1], [-0.1, 5.1] , [-0.1, 5.1], [-0.1, 6.1] ,[-0.1, 5.1]};
+config.ylim = {[-22, 102], [-22, 102], [-22, 102], [-0.1, 5.1] , [-0.1, 5.1], [-0.1, 6.1] ,[-0.1, 5.1]};
 config.units = {'cm/s', 'cm/s', 'cm/s', 'V', 'V', 'V', 'V'};
