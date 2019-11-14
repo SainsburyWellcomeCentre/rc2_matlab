c35014b28a7942180e3cc876fb4a00a846a440b4
lcossell <l.cossell@ucl.ac.uk>
2019-11-14 20:15:35 +0000

    improve documetation for Reward class

--- rc/actions/Reward.m
+++ rc/actions/Reward.m
@@ -1,14 +1,11 @@
 classdef Reward < handle
     
+    
     properties
-        
-        pump
         
         randomize
         min_time
         max_time
-        
-        rand_timer
     end
     
     properties (SetObservable = true, SetAccess = private)
@@ -20,31 +17,53 @@ classdef Reward < handle
         
         min_duration = 1;
         max_duration = 500;
+        rand_timer
     end
     
+    properties (Hidden = true)
+        
+        pump
+    end
     
     
     methods
         
         function obj = Reward(pump, config)
-            
+        %%obj = REWARD(pump, config)
+        %   Main class for controlling presentation of reward.
+        %       Acts on the pump class.
+        
             obj.pump = pump;
             
+            % control the randomization of the reward
             obj.randomize = config.reward.randomize;
             obj.min_time = config.reward.min_time;
             obj.max_time = config.reward.max_time;
+            
+            % duration
             obj.duration = config.reward.duration;
         end
         
         
         function start_reward(obj, wait_for_reward)
-            
+        %%START_REWARD(obj, wait_for_reward)
+        %   Starts the timing of the reward, or just presents the reward.
+        %       This class should be used for all presentation of rewards.
+        %       If an immediate reward is required set wait_for_reward to
+        %       zero.
+        
+            % interval to wait before presentation of reward
             if ~obj.randomize
+                
+                % set the interval to minimum interval.
                 interval = 0.01;
             else
+                
+                % interval is a random time between the min and max
                 interval = obj.min_time + (obj.max_time-obj.min_time)*rand;
                 interval = round(interval*100)/100;
             end
+            
             
             % delete old timers
             if ~isempty(obj.rand_timer)
@@ -53,12 +72,17 @@ classdef Reward < handle
                 end
             end
             
+            
             % whether to wait for reward to complete before returning
             if wait_for_reward
+                
+                % this will block... could reimplement as tic/toc loop
                 pause(interval)
                 obj.give_reward()
-                pause(obj.duration*1e-3+0.5)
+                pause(obj.duration*1e-3 + 0.5) % 
             else
+                
+                % start a timer to give reward after a certain interval
                 obj.rand_timer = timer();
                 obj.rand_timer.StartDelay = interval;
                 obj.rand_timer.TimerFcn = @(src, evt)obj.give_reward(src, evt);
@@ -67,18 +91,34 @@ classdef Reward < handle
         end
         
         
-        function give_reward(obj, ~, ~)
-            obj.pump.pulse(obj.duration);
-        end
-        
-        
         function status = set_duration(obj, val)
+        %%status = SET_DURATION(obj, val)
+        %   Sets the duration of the reward (i.e. pump on)
+        %   Inputs:
+        %       val - value to use for the duration in milliseconds.
+        %   Outputs:
+        %       status - 0 on success, 1 on failure
+        
+            % return status
             status = 0;
             if val <= obj.min_duration || val > obj.max_duration
                 status = -1;
                 return
             end
+            
+            % set the duration
             obj.duration = val;
+        end
+    end
+    
+    
+    methods (Access = private)
+        
+        function give_reward(obj, ~, ~)
+        %%GIVE_REWARD(obj, ~, ~)
+        %   Set the pump to pulse for obj.duration milliseconds.
+        
+            obj.pump.pulse(obj.duration);
         end
     end
 end
