d9b78b8b2305d02f39a27a02eff824c96c271544
lcossell <l.cossell@ucl.ac.uk>
2019-11-14 20:28:03 +0000

    improve documentation for trigger input class

--- rc/actions/TriggerInput.m
+++ rc/actions/TriggerInput.m
@@ -1,7 +1,12 @@
 classdef TriggerInput < handle
     
-    properties
+    properties (Hidden = true)
+        
         ni
+    end
+    
+    properties (Hidden = true, SetAccess = private)
+        
         teensy_channel
         soloist_channel
         current_channel
@@ -9,17 +14,28 @@ classdef TriggerInput < handle
     
     methods
         function obj = TriggerInput(ni, config)
+        %%obj = TRIGGERINPUT(ni, config)
+        %   There is a single trigger input class which listens to either
+        %   of two digital inputs. We could potentially set up two separate
+        %   objects, but deal with both in the same object.
+        
             obj.ni = ni;
             
+            % The name of the digital input channel is
             all_channel_names = obj.ni.di_names();
             obj.teensy_channel = find(strcmp('from_teensy', all_channel_names));
             obj.soloist_channel = find(strcmp('from_soloist', all_channel_names));
             
+            % We are initially listening on the input set in config.
             obj.listen_to(config.trigger_input.init_source);
         end
         
         
         function listen_to(obj, src)
+        %%LISTEN_TO(obj, src)
+        %   Set the current channel to listen to one of the two inputs.
+        %   This could be made more generic.
+        
             if strcmp(src, 'teensy')
                 obj.current_channel = obj.teensy_channel;
             elseif strcmp(src, 'soloist')
@@ -29,6 +45,9 @@ classdef TriggerInput < handle
         
         
         function data = read(obj)
+        %%data = READ(obj)
+        %   Read the state of the channel we are currently listening to.
+        
             data = obj.ni.di.read_channel(obj.current_channel);
         end
     end
