ade3617d8c44041673392e77e8502934f4ede002
lcossell <l.cossell@ucl.ac.uk>
2019-11-14 20:51:19 +0000

    improved documentation for class to transform data

--- rc/aux_/DataTransform.m
+++ rc/aux_/DataTransform.m
@@ -10,14 +10,27 @@ classdef DataTransform < handle
     methods
         
         function obj = DataTransform(config)
-            
+        %%obj = DATATRANSFORM(config)
+        %   Stores information for and performs a transformation of voltage
+        %   signals coming into the NIDAQ to useful scales (e.g. V to cm/s).
+        %       offset and scale are 1 x n vectors containing the offset to
+        %       subtract and the scale to MULTIPLY by for each of the n
+        %       channels in data.
+        
             obj.offset = config.nidaq.ai.offset;
             obj.scale = config.nidaq.ai.scale;
         end
         
         
         function data = transform(obj, data)
-            
+        %%data = TRANSFORM(obj, data)
+        %   Transform the data according to an offset and a scale.
+        %   Inputs:
+        %       data - the "raw" data.
+        %   Outputs:
+        %       data - data after offset subracted and then scaled (in that
+        %       order)
+        
             data = bsxfun(@minus, data, obj.offset);
             data = bsxfun(@times, data, obj.scale);
         end
