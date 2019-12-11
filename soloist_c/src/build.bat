8ff48a8cafc39dcdd6012f5eeffcea814c032aa1
lcossell <l.cossell@ucl.ac.uk>
2019-12-11 19:56:29 +0000

    update Kpos in header and change build.bat

--- soloist_c/src/build.bat
+++ soloist_c/src/build.bat
@@ -4,4 +4,5 @@ g++ -o ..\exe\home.exe home.c rc_shared.c -L"C:/Program Files (x86)/Aerotech/Sol
 g++ -o ..\exe\reset.exe reset.c rc_shared.c -L"C:/Program Files (x86)/Aerotech/Soloist/CLibrary/Lib64" -lSoloistC64
 g++ -o ..\exe\listen_until.exe listen_until.c rc_shared.c -L"C:/Program Files (x86)/Aerotech/Soloist/CLibrary/Lib64" -lSoloistC64
 g++ -o ..\exe\move_to.exe move_to.c rc_shared.c -L"C:/Program Files (x86)/Aerotech/Soloist/CLibrary/Lib64" -lSoloistC64
+g++ -o ..\exe\calibrate_zero.exe calibrate_zero.c rc_shared.c -L"C:/Program Files (x86)/Aerotech/Soloist/CLibrary/Lib64" -lSoloistC64
 echo done
