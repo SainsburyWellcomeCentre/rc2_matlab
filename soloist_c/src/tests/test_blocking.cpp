77ac868549e6860e618fc603ad5acf69f52edac3
lcossell <l.cossell@ucl.ac.uk>
2019-12-16 15:09:17 +0000

    test of running ab script from c++ ok

--- soloist_c/src/tests/test_blocking.cpp
+++ soloist_c/src/tests/test_blocking.cpp
@@ -1,4 +1,4 @@
-#include "rc_soloist.h"
+#include "..\rc_soloist.h"
 #include <stdio.h>
 #include <tchar.h>
 
@@ -8,24 +8,25 @@ Compile with (or similar)
 g++ -o test_blocking.exe test_blocking.cpp rc_shared.c -L"C:/Program Files (x86)/Aerotech/Soloist/CLibrary/Lib64" -lSoloistC64
 */
 
-int 
-
 int
 main(int argc, char **argv)
 {
     SoloistHandle *handles;
 	DWORD handle_count = 0;
-	//int task_state;
-	
+	TASKSTATE task_state;
+    
 	// Location of the test aerobasic script
 	LPCSTR ab_script = "..\\..\\ab\\tests\\test_blocking.ab";
 	
 	if(!SoloistConnect(&handles, &handle_count)) { cleanup(handles, handle_count); }
-	if(!SoloistProgramLoad(handles[0], 1, ab_script)) { cleanup(handles, handle_count); }
+	if(!SoloistProgramLoad(handles[0], TASKID_01, ab_script)) { cleanup(handles, handle_count); }
 	printf("Starting the aerobasic program\n");
-	if(!SoloistProgramStart(handles[0], 1)) { cleanup(handles, handle_count); }
+	if(!SoloistProgramStart(handles[0], TASKID_01)) { cleanup(handles, handle_count); }
 	
-	//SoloistProgramGetTaskState(handles[0], 1, &task_state);
+    SoloistProgramGetTaskState(handles[0], TASKID_01, &task_state);
+    while (task_state!=TASKSTATE_ProgramComplete) {
+        SoloistProgramGetTaskState(handles[0], TASKID_01, &task_state);
+    }
 	//printf("Task state: %i\n", task_state);
 	
 	printf("Test result:  it has not/has blocked\n");
