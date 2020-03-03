9f6f444fcc62b247e39c1827be0c40abb4d0a6b2
lcossell <l.cossell@ucl.ac.uk>
2020-03-03 20:45:36 +0000

    plot photodiode, not gain

--- rc/aux_/plotting_config.m
+++ rc/aux_/plotting_config.m
@@ -5,7 +5,7 @@ function config = plotting_config()
 
 config.time = 5; %s
 config.fig.position = [1340, 80, 1333, 630];
-config.chans_to_plot = [1:6, 8];
+config.chans_to_plot = [1:6, 7];
 config.ylim = {[-22, 102], [-22, 102], [-22, 102], [-0.1, 5.1] , [-0.1, 5.1], [-0.2, 5.1] ,[-0.1, 5.1]};
 config.units = {'cm/s', 'cm/s', 'cm/s', 'V', 'V', 'V', 'V'};
 config.ax_positions = {[0.03, 0.54, 0.45, 0.19], ...
