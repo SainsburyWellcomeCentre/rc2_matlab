0242590148b8a5f89c7dcb88fd6a9650aef93759
lcossell <l.cossell@ucl.ac.uk>
2020-09-29 08:52:45 +0100

    code for head-tilt protocol

--- rc/user/protocols/head_tilt_mice/create_head_tilt_protocol_sequence.m
+++ rc/user/protocols/head_tilt_mice/create_head_tilt_protocol_sequence.m
@@ -1,4 +1,4 @@
-function [trial_order, protocol_id, file_no] = create_head_tilt_protocol_sequence()
+function [trial_order, fnames, protocol_id] = create_head_tilt_protocol_sequence()
 % Create trial sequence for the "head-tilt" mice. This consists of:
 %
 %   1. vestibular motion with visual flow 
@@ -19,29 +19,88 @@ function [trial_order, protocol_id, file_no] = create_head_tilt_protocol_sequenc
 
 
 % restart random number generator
-%   rng(1) gives too many 3's at the beginning
-rng(2)
+rng(1)
 
-% number of velocity profiles
-n_batches = 20;
+% data location
+data_dir = fullfile(pwd, 'head_tilt_waveforms');
+
+% location of waveform presentation order
+vis_flow_list_fname = fullfile(data_dir, 'visual_flow_presentation_order.txt');
+darkness_list_fname = fullfile(data_dir, 'darkness_presentation_order.txt');
+
+% create fid
+fid_vis_flow = fopen(vis_flow_list_fname, 'r');
+fid_darkness = fopen(darkness_list_fname, 'r');
+
+% read presentation orders
+vis_flow_str = fread(fid_vis_flow, '*char')';
+darkness_str = fread(fid_darkness, '*char')';
+
+% close files
+fclose(fid_vis_flow);
+fclose(fid_darkness);
+
+% split the strings
+vis_flow_fnames = strsplit(vis_flow_str, ',');
+darkness_fnames = strsplit(darkness_str, ',');
+
+% prepend the data location
+vis_flow_fnames = cellfun(@(x)(fullfile(data_dir, x)), vis_flow_fnames, 'uniformoutput', false);
+darkness_fnames = cellfun(@(x)(fullfile(data_dir, x)), darkness_fnames, 'uniformoutput', false);
 
 % list of protocols
 protocol_id.vest_with_flow      = 1;
-protocol_id.vest_darkness       = 2;
-protocol_id.visual_flow         = 3;
+protocol_id.visual_flow         = 2;
+protocol_id.vest_darkness       = 3;
+
+% number of trials in each protocol
+n_trials = [length(vis_flow_fnames), length(vis_flow_fnames), length(darkness_fnames)];
 
 % 
-trial_order_ = repmat(1:3, n_batches, 1);
-file_no_ = [randperm(n_batches)', randperm(n_batches)', randperm(n_batches)'];
+trial_order_ = repmat((1:3)', 1, max(n_trials));
+file_no_ = repmat((1:max(n_trials)), 3, 1);
 
 
-trial_order = nan(3, n_batches);
-file_no = nan(3, n_batches);
+for i = 1 : 3
+    trial_order_(i, (n_trials(i)+1):end) = nan;
+    file_no_(i, (n_trials(i)+1):end) = nan;
+end
 
-for i = 1 : n_batches
-    
+% randomize across repeats
+for i = 1 : 3
+    I = randperm(max(n_trials));
+    trial_order_(i, :) = trial_order_(i, I);
+    file_no_(i, :) = file_no_(i, I);
+end
+
+
+% randomize across batches
+for i = 1 : max(n_trials)
     I = randperm(3);
-    trial_order(:, i) = I;
-    file_no(:, i) = file_no_(i, I);
+    trial_order_(:, i) = trial_order_(I, i);
+    file_no_(:, i) = file_no_(I, i);
+end
+
+
+% vectorize the order
+trial_order = trial_order_(:);
+file_no = file_no_(:);
+
+% remove nan values
+trial_order(isnan(trial_order)) = [];
+file_no(isnan(file_no)) = [];
+
+% 
+fnames = cell(length(file_no), 1);
+
+% 
+for i = 1 : length(trial_order)
     
+    if trial_order(i) == protocol_id.vest_with_flow
+        fnames{i} = fullfile(data_dir, vis_flow_fnames{file_no(i)});
+    elseif trial_order(i) == protocol_id.visual_flow
+        fnames{i} = fullfile(data_dir, vis_flow_fnames{file_no(i)});
+    elseif trial_order(i) == protocol_id.vest_darkness
+        fnames{i} = fullfile(data_dir, darkness_fnames{file_no(i)});
+    end
 end
--- rc/user/protocols/head_tilt_mice/head_tilt_protocol.m
+++ rc/user/protocols/head_tilt_mice/head_tilt_protocol.m
@@ -1,6 +1,6 @@
 function seq = head_tilt_protocol(ctl)
 
-load('head_tilt_protocol_sequence.mat', 'trial_order', 'protocol_id');
+load('head_tilt_protocol_sequence.mat', 'trial_order', 'protocol_id', 'fnames');
 % "trial_order" is a 3xN matrix of protocol indices. Each column is a batch
 % of trials specifying a particular order (1-4 in some order that makes sense).
 % "protocol_id" is a structure which relates the numbers in "trial_order"
@@ -29,89 +29,73 @@ seq.randomize_reward = true;
 
 
 % insert all the trials
-for i = 1 : size(trial_order, 2)
+for i = 1 : length(trial_order)
     
-    for j  = 1 : size(trial_order, 1)
+    if trial_order(i) == protocol_id.vest_with_flow
         
-        if trial_order(j, i) == protocol_id.vest_with_flow
-            
-            % setup the locovest replay protocol
-            vest = StageOnly(ctl, config);
-            
-            % mouse must initiate trial
-            vest.initiate_trial = true;
-            
-            % increase start dwell time slightly
-            vest.start_dwell_time = 6;
-            
-            % same filename as loco
-            fname_ = sprintf('%s_%s_%03i_single_trial_prot_%02i_%03i.bin', ...
-                ctl.saver.prefix, ...
-                ctl.saver.suffix, ...
-                ctl.saver.index, ...
-                protocol_id.loco, ...
-                i);
-            
-            vest.wave_fname = fullfile(ctl.saver.save_to, ctl.saver.prefix, fname_);
-            
-            % enable vis stim
-            vest.enable_vis_stim = true;
-            
-            % add protocol to the sequence
-            seq.add(vest);
-            
-        elseif trial_order(j, i) == protocol_id.vest_darkness
-            
-            % setup the locovest replay protocol
-            vest = StageOnly(ctl, config);
-            
-            % mouse must initiate trial
-            vest.initiate_trial = true;
-            
-            % increase start dwell time slightly
-            vest.start_dwell_time = 6;
-            
-            % same filename as loco
-            fname_ = sprintf('%s_%s_%03i_single_trial_prot_%02i_%03i.bin', ...
-                ctl.saver.prefix, ...
-                ctl.saver.suffix, ...
-                ctl.saver.index, ...
-                protocol_id.loco, ...
-                i);
-            
-            vest.wave_fname = fullfile(ctl.saver.save_to, ctl.saver.prefix, fname_);
-            
-            % enable vis stim
-            vest.enable_vis_stim = false;
-            
-            % add protocol to the sequence
-            seq.add(vest);
-            
-            
-        elseif trial_order(j, i) == protocol_id.visual_flow
-            
-            % setup the locovest protocol
-            replay = ReplayOnly(ctl, config);
-            
-            % mouse must initiate trial
-            replay.initiate_trial = true;
-            
-            % increase start dwell time slightly
-            replay.start_dwell_time = 6;
-            
-            % same filename as locovest
-            fname_ = sprintf('%s_%s_%03i_single_trial_prot_%02i_%03i.bin', ...
-                ctl.saver.prefix, ...
-                ctl.saver.suffix, ...
-                ctl.saver.index, ...
-                protocol_id.locovest, ...
-                i);
-            
-            replay.wave_fname = fullfile(ctl.saver.save_to, ctl.saver.prefix, fname_);
-            
-            % add protocol to the sequence
-            seq.add(replay);
-            
-        end
+        % setup the locovest replay protocol
+        vest = StageOnly(ctl, config);
+        
+        % mouse must initiate trial
+        vest.initiate_trial = false;
+        
+        % increase start dwell time slightly
+        vest.start_dwell_time = 6;
+        
+        fname_ = fnames{i};
+        
+        vest.wave_fname = fullfile(ctl.saver.save_to, ctl.saver.prefix, fname_);
+        
+        % enable vis stim
+        vest.enable_vis_stim = true;
+        
+        % add protocol to the sequence
+        seq.add(vest);
+        
+    elseif trial_order(i) == protocol_id.vest_darkness
+        
+        % setup the locovest replay protocol
+        vest = StageOnly(ctl, config);
+        
+        % mouse must initiate trial
+        vest.initiate_trial = false;
+        
+        % increase start dwell time slightly
+        vest.start_dwell_time = 6;
+        
+        % same filename as loco
+        fname_ = fnames{i};
+        
+        vest.wave_fname = fullfile(ctl.saver.save_to, ctl.saver.prefix, fname_);
+        
+        % enable vis stim
+        vest.enable_vis_stim = false;
+        
+        % add protocol to the sequence
+        seq.add(vest);
+        
+        
+    elseif trial_order(i) == protocol_id.visual_flow
+        
+        % setup the locovest protocol
+        replay = ReplayOnly(ctl, config);
+        
+        % mouse must initiate trial
+        replay.initiate_trial = false;
+        
+        % increase start dwell time slightly
+        replay.start_dwell_time = 6;
+        
+        % same filename as locovest
+        fname_ = fnames{i};
+        
+        replay.wave_fname = fullfile(ctl.saver.save_to, ctl.saver.prefix, fname_);
+        
+        % enable vis stim
+        vest.enable_vis_stim = true;
+        
+        % add protocol to the sequence
+        seq.add(replay);
+        
     end
 end
