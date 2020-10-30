f50b98d1bbda96cfd1f892e03e8a9bd336a14316
lcossell <l.cossell@ucl.ac.uk>
2020-10-30 10:51:46 +0000

    look for config file in correct location on startup

--- rc/aux_/load_config.m
+++ rc/aux_/load_config.m
@@ -2,7 +2,7 @@ function config = load_config()
 
 % select config file
 this_dir = fileparts(mfilename('fullpath'));
-fname = uigetfile(fullfile(this_dir, 'main', 'configs', '*.m'), 'Load configuration file...');
+fname = uigetfile(fullfile(this_dir, '..', 'main', 'configs', '*.m'), 'Load configuration file...');
 if fname == 0; fprintf('No config selected\n'); return; end
 
 % load config file
