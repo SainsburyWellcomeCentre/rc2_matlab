4834bd73bf6d4886dcaadef10fa3348816aee006
treadmill <l.cossell@ucl.ac.uk>
2021-08-04 14:57:36 +0100

    ask for animal information on rc2_3p_startup.m

--- rc/user/3p/rc2_3p_startup.m
+++ rc/user/3p/rc2_3p_startup.m
@@ -1,20 +1,28 @@
 % saving Analog Input
 config = config_default_3P(false);
-ctl = Controller(config);
+%%
 
-ctl.saver.save_to = 'D:\Data\Default3PData';
+ctl = Controller(config);
+%%
+ctl.saver.save_to = 'D:\Data\3PData';
 
 % Mouse ID
-ctl.saver.prefix = 'CAA-1111111';
-
-% Experiment type and number (NOTE: must manually increment number each
-% time!)
-ctl.saver.suffix = 'blank-1';
+animalid = 'Animals ID:';
+ctl.saver.prefix = input(animalid);
+%ctl.saver.prefix = 'Test_210628SW_3';
+% Experiment type/date and number
+expnr='Date and exp name:';
+ctl.saver.suffix = input(expnr);
 %ctl.saver.suffix = 'binoc-1';
 %ctl.saver.suffix = 'left-1';
 %ctl.saver.suffix = 'right-1';
 
-
+%%
 ctl.prepare_acq();
+
+%%
 ctl.start_acq();
+
+% start vis stim
+
   
