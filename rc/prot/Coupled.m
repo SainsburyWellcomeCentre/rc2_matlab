98574f69dbeed2cbbd05fdde5fb949013820dd7e
lcossell <l.cossell@ucl.ac.uk>
2022-01-17 14:47:11 +0000

    another change in the offset due to solenoid state

--- rc/prot/Coupled.m
+++ rc/prot/Coupled.m
@@ -14,7 +14,7 @@ classdef Coupled < handle
         log_trial = false
         log_fname = ''
         
-        solenoid_correction = 0.9500 % 2.35  % millivolts
+        solenoid_correction = 1.55 % 2.35  % millivolts
     end
     
     properties (SetAccess = private)
