46faf4a97860fd5d1df4ea7978099f2d53ac0bd9
RoboDoig <erskine94@gmail.com>
2023-06-28 18:22:35 +0100

    Add comments to gain_seq

--- rc/user/protocols/shelter/shelter_test.m
+++ rc/user/protocols/shelter/shelter_test.m
@@ -5,7 +5,7 @@ function seq = shelter_test(ctl)
     % setup the protocol sequence
     seq = ProtocolSequence(ctl);
     nTrials = 5;
-    gain_seq = {[1, 1], [0, 1], [1, 1], [1, 0], [0, 0]}; % Defines the gain pins high or low. e.g. [1, 1] is both pins high = default gain. [0, 0] is both bins low = zero gain
+    gain_seq = {[1, 1], [0, 1], [1, 1], [1, 0], [0, 0]}; % Defines the gain pins high or low. e.g. [1, 1] is both pins high = default gain. [0, 0] is both bins low = zero gain. [1, 0] is HIGH high = max gain.
     
     assert(nTrials == length(gain_seq), 'Warning: number of trials does not match the number of gain factors');
     
