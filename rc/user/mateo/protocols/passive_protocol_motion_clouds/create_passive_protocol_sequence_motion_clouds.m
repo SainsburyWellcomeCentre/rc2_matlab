85cf1562581b241c99d5ae529b9c071d1de583ad
velezmat <m.velez-fort@ucl.ac.uk>
2025-04-25 11:01:45 +0100

    Created a new protocol to keep it clean

---
+++ rc/user/mateo/protocols/passive_protocol_motion_clouds/create_passive_protocol_sequence_motion_clouds.m
@@ -0,0 +1,59 @@
+function [trial_order, fnames, protocol_id] = create_passive_protocol_sequence_motion_clouds()
+% Create trial sequence for the "passive" mice. This consists of:
+%
+%   1. vestibular motion with visual flow 
+%   2. vestibular motion with visual static Tvs
+%   3. visual flow (no vestibular motion)
+%
+%   All motion comes from saved velocity profiles
+%
+%   The sequence occurs in batches (i.e. each type of trial is played once,
+%   before the next batch)
+%   Within each batch the trials are randomized.
+%   
+%   Files are randomized across *all* trials.
+%
+%   trial_order - sequence of trials
+%   protocol_id - structure containing ID of each trial type
+
+
+
+% restart random number generator
+rng(1)
+
+% data location
+data_dir = fullfile(pwd, 'passive_waveforms_motion_clouds');
+
+track_fnames = dir(fullfile(data_dir, '*.bin'));
+track_fnames = {track_fnames(:).name};
+
+
+% list of protocols
+protocol_id.vest_with_flow      = 1;
+protocol_id.visual_flow         = 2;
+protocol_id.vest_darkness       = 3;
+
+% number of trials in each protocol
+n_trials = length(track_fnames);
+
+trial_order = [ones(1, n_trials); 2*ones(1, n_trials); 3*ones(1, n_trials)];
+file_id = nan(3, n_trials);
+
+for i = 1 : 3    
+    file_id(i, :) = randperm(n_trials);    
+end
+
+
+for i = 1 : n_trials
+    I = randperm(3);
+    trial_order(:, i) = trial_order(I, i);
+    file_id(:, i) = file_id(I, i);
+end
+
+trial_order = trial_order(:);
+fnames = track_fnames(file_id(:))';
+
+% create full path to file
+fnames = cellfun(@(x)(fullfile(data_dir, x)), fnames, 'uniformoutput', false);
+
+save('passive_protocol_sequence_motion_clouds.mat', 'fnames', 'protocol_id', 'trial_order');
---
+++ rc/user/mateo/protocols/passive_protocol_motion_clouds/passive_protocol_motion_clouds.m
@@ -0,0 +1,94 @@
+function seq = passive_protocol_motion_clouds(ctl)
+
+load('passive_protocol_sequence_motion_clouds.mat', 'trial_order', 'protocol_id', 'fnames');
+% "trial_order" is a 3xN matrix of protocol indices. Each column is a batch
+% of trials specifying a particular order (1-3 in some order that makes sense).
+% "protocol_id" is a structure which relates the numbers in "trial_order"
+% to the protocol by name:
+%       vest_with_flow
+%       vest_darkness (in fact, visual static Tvs)
+%       visual_flow
+
+
+% positions along stage
+reward_position     = 250;
+distance            = 1200;
+back_distance       = 5;
+
+% config parameters to pass to the protocols
+config.stage.start_pos      = reward_position + distance;
+config.stage.back_limit     = config.stage.start_pos + back_distance;
+config.stage.forward_limit  = reward_position;
+
+
+% create the protocol sequence
+seq = ProtocolSequence(ctl);
+
+% we will randomize the rewards
+seq.randomize_reward = true;
+
+
+% insert all the trials
+for i = 1 : length(trial_order)
+    
+    if trial_order(i) == protocol_id.vest_with_flow
+        
+        % setup the locovest replay protocol
+        vest = StageOnly(ctl, config);
+        
+        % mouse must initiate trial
+        vest.initiate_trial = false;
+        
+        % increase start dwell time slightly
+        vest.start_dwell_time = 4;
+        
+        vest.wave_fname = fnames{i};
+        
+        % enable vis stim
+        vest.enable_vis_stim = true;
+        vest.enable_vis_stim_gain = true;
+        
+        % add protocol to the sequence
+        seq.add(vest);
+        
+    elseif trial_order(i) == protocol_id.vest_darkness % TODO change name to appropriate protocol ID
+        
+        % setup the locovest replay protocol
+        vest = StageOnly(ctl, config);
+        
+        % mouse must initiate trial
+        vest.initiate_trial = false;
+        
+        % increase start dwell time slightly
+        vest.start_dwell_time = 4;
+        
+        vest.wave_fname = fnames{i};
+        
+        % enable vis stim
+        vest.enable_vis_stim = true;
+        vest.enable_vis_stim_gain = false;
+        
+        % add protocol to the sequence
+        seq.add(vest);
+        
+    elseif trial_order(i) == protocol_id.visual_flow
+        
+        % setup the locovest protocol
+        replay = ReplayOnly(ctl, config);
+        
+        % mouse must initiate trial
+        replay.initiate_trial = false;
+        
+        % increase start dwell time slightly
+        replay.start_dwell_time = 4;
+        
+        replay.wave_fname = fnames{i};
+        
+        % enable vis stim
+        replay.enable_vis_stim = true;
+        
+        % add protocol to the sequence
+        seq.add(replay);
+        
+    end
+end
